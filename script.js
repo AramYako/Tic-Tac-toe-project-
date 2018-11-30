763926bd982e83ba97be6e9a8a80ce42826d7ad4
AramYako <Aramyako@hotmail.com>
2018-11-30 10:17:42 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onload = function () {              // we can use to perform some task as soon as the page finishes loading.
+ var num;
+ var box; 
+ var ctx; 
+ var turn=1; 
+ var filled; 
+ var symbol; 
+ var winner; 
+ var gameOver=false;
+ var human = "X";
+ var ai="o";
+ var result= {};  
